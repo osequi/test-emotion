3529c87b4c7c40ef9fb1fab92533e4f9ff5f8c99
osequi <cs@osequi.com>
2020-10-13 19:24:36 +0300

    Working on examples

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { css } from "emotion";
+import { cx, css } from "emotion";
 
 const simple = {
   backgroundColor: "red",
@@ -9,6 +9,22 @@ const props1 = (props) => {
   return {
     backgroundColor: props.backgroundColor,
   };
+};
+
+const list = (props) => {
+  return {
+    ["& :nth-child(1)"]: {
+      backgroundColor: "green",
+    },
+
+    [`${props.selector}`]: {
+      backgroundColor: "blue",
+    },
+  };
+};
+
+const white = {
+  color: "white",
 };
 
 const App = () => {
@@ -23,6 +39,16 @@ const App = () => {
       <div className={css(props1(props))}>
         Backgroud is yellow. Set by props.
       </div>
+      <ul className={css(list(props))}>
+        <li>& :nth-child(1) should be green</li>
+        <li>& :nth-child(2) should be blue.</li>
+        <li>xxx</li>
+      </ul>
+      <ul className={cx(css(list(props)), css(white))}>
+        <li>& :nth-child(1) should be green, color white</li>
+        <li>& :nth-child(2) should be blue, color white</li>
+        <li>xxx</li>
+      </ul>
     </>
   );
 };
