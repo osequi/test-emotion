7a9c76307ee4398fef4c0cab55eb42fd182c6d10
osequi <cs@osequi.com>
2020-10-13 19:19:15 +0300

    Working on examples

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { css } from "emotion";
 
-const simple = css({
+const simple = {
   backgroundColor: "red",
-});
+};
+
+const props1 = (props) => {
+  return {
+    backgroundColor: props.backgroundColor,
+  };
+};
 
 const App = () => {
+  const props = {
+    backgroundColor: "yellow",
+    selector: "& :nth-child(2)",
+  };
+
   return (
     <>
-      <div className={simple}>Background is red</div>
+      <div className={css(simple)}>Background is red</div>
+      <div className={css(props1(props))}>
+        Backgroud is yellow. Set by props.
+      </div>
     </>
   );
 };
