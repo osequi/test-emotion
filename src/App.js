bf647ca673f4dd755dda499e3ae6f2472ca6d6ec
osequi <cs@osequi.com>
2020-10-13 19:54:07 +0300

    Working on the DX

--- src/App.js
+++ src/App.js
@@ -2,14 +2,35 @@ import React from "react";
 import { cx, css } from "emotion";
 
 const simple = {
+  label: "Simple",
   backgroundColor: "red",
 };
+
+const cssSimple = css(simple);
+
+const white = {
+  label: "White",
+  color: "white",
+};
+
+const cssWhite = css(white);
+
+const composed = {
+  ...simple,
+  ...white,
+  label: "Composed",
+};
+
+const cssComposed = css(composed);
 
 const props1 = (props) => {
   return {
     backgroundColor: props.backgroundColor,
   };
 };
+
+// NOTE: This is ok, but far from a good DX. See below when using it.
+const cssProps1 = (props) => css(props1(props));
 
 const list = (props) => {
   return {
@@ -23,9 +44,7 @@ const list = (props) => {
   };
 };
 
-const white = {
-  color: "white",
-};
+const cssList = css(list);
 
 const App = () => {
   const props = {
@@ -33,18 +52,27 @@ const App = () => {
     selector: "& :nth-child(2)",
   };
 
+  //// NOTE: a useStyles(props) hooks is very missed here ....
+
   return (
     <>
+      <h1>Gotchas</h1>
+      <ol>
+        <li>Always use `cx`. It includes the `label`</li>
+      </ol>
       <div className={css(simple)}>Background is red</div>
+      <div className={cssSimple}>Background is red. Set without `css`</div>
+      <div className={css(composed)}>Background is red, color is white</div>
       <div className={css(props1(props))}>
         Backgroud is yellow. Set by props.
       </div>
+      <div className={cssProps1(props)}>Backgroud is yellow. Set by props.</div>
       <ul className={css(list(props))}>
         <li>& :nth-child(1) should be green</li>
         <li>& :nth-child(2) should be blue.</li>
         <li>xxx</li>
       </ul>
-      <ul className={cx(css(list(props)), css(white))}>
+      <ul className={cx(css(list(props)), css(white), "Cx")}>
         <li>& :nth-child(1) should be green, color white</li>
         <li>& :nth-child(2) should be blue, color white</li>
         <li>xxx</li>
